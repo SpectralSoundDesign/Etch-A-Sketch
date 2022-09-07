8f5bf7865204e0dae3d61b3ce55ac838474baecc
Will Richards <spectralsounddesign@gmail.com>
2022-09-07 13:42:14 -0400

    formatted code

--- main.js
+++ main.js
@@ -1,35 +1,31 @@
 let pixels = prompt("Pixels per side");
 let numOfDivs = pixels * pixels;
 
-const body = document.querySelector('body');
-const container = document.querySelector('.container');
-const resetBtn = document.querySelector('.reset-button');
+const body = document.querySelector("body");
+const container = document.querySelector(".container");
+const resetBtn = document.querySelector(".reset-button");
 
-
-
-function createBoxes(numOfDivs){
-    for (let i = 0; i < numOfDivs; i++) {
-        let boxes = document.createElement('div');
-        boxes.style.background = 'yellow';
-        boxes.style.width = `${800/pixels}px`;
-        boxes.style.height = `${800/pixels}px`;
-        boxes.classList.add("box-class");
-        container.appendChild(boxes);
-    }
+function createBoxes(numOfDivs) {
+  for (let i = 0; i < numOfDivs; i++) {
+    let boxes = document.createElement("div");
+    boxes.style.background = "yellow";
+    boxes.style.width = `${800 / pixels}px`;
+    boxes.style.height = `${800 / pixels}px`;
+    boxes.classList.add("box-class");
+    container.appendChild(boxes);
+  }
 }
 
 createBoxes(numOfDivs);
 
-const allBoxes = document.querySelectorAll('.box-class');
+const allBoxes = document.querySelectorAll(".box-class");
 
 allBoxes.forEach((eachBox) => {
-    eachBox.addEventListener('mouseover', () => {
-        eachBox.style.background = 'purple';
-    });
+  eachBox.addEventListener("mouseover", () => {
+    eachBox.style.background = "purple";
+  });
 });
 
-resetBtn.addEventListener('click', () => {
-    window.location.reload();
+resetBtn.addEventListener("click", () => {
+  window.location.reload();
 });
-
-
