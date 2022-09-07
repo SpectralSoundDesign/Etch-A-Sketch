513be05b7f02e36e6907c705f2b50f59091f0d5a
Will Richards <spectralsounddesign@gmail.com>
2022-09-07 13:40:21 -0400

    added etch-a-sketch functionality

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
-const numOfDivs = 256;
+let pixels = prompt("Pixels per side");
+let numOfDivs = pixels * pixels;
 
 const body = document.querySelector('body');
 const container = document.querySelector('.container');
+const resetBtn = document.querySelector('.reset-button');
+
 
 
 function createBoxes(numOfDivs){
     for (let i = 0; i < numOfDivs; i++) {
         let boxes = document.createElement('div');
         boxes.style.background = 'yellow';
-        boxes.style.width = '50px';
-        boxes.style.height = '50px';
+        boxes.style.width = `${800/pixels}px`;
+        boxes.style.height = `${800/pixels}px`;
         boxes.classList.add("box-class");
         container.appendChild(boxes);
     }
@@ -23,6 +26,10 @@ allBoxes.forEach((eachBox) => {
     eachBox.addEventListener('mouseover', () => {
         eachBox.style.background = 'purple';
     });
+});
+
+resetBtn.addEventListener('click', () => {
+    window.location.reload();
 });
 
 
